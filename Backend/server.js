31906eda3f995239626733caf58b9854d350ee63
bharathraj49 <bharath04092004gmail.com>
2026-02-25 23:25:44 +0530

    Add user registration and login with JWT authentication

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,25 @@
-const express= require('express');
-const app=express();
+const express = require('express');
+const mongoose = require('mongoose');
+const userRoutes = require('./routes/userRoutes');
 require('dotenv').config();
 
-const PORT=process.env.PORT||5000;
-app.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}`);
-})
+const app = express();
+
+
+app.use(express.json());
+
+app.use('/api/users', userRoutes);
+
+const PORT = process.env.PORT || 5010;
+
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('Connected to MongoDB');
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error('Error connecting to MongoDB', err);
+  });
